37e1509f729960b097a7ac60bc11fa5c78b9a000
Gergely Szalay <gergely.szalay@liferay.com>
2023-01-25 18:39:08 +0100

    added classes

--- src/chessrmi/GameWindow.java
+++ src/chessrmi/GameWindow.java
@@ -15,69 +15,83 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
 import static javax.swing.SwingConstants.TOP;
+
 import javax.swing.border.MatteBorder;
 
 /**
- *
  * @author Gege
  */
 public class GameWindow {
+    private static GameWindow gameWindow=null;
+    public static synchronized GameWindow getInstance(){
+        if (gameWindow ==null){
+            gameWindow = new GameWindow();
+        }
+        return gameWindow;
+    }
     javax.swing.JLabel jLabel1 = new javax.swing.JLabel();
-javax.swing.JLabel jLabel2 = new javax.swing.JLabel();
-javax.swing.JLabel jLabel3 = new javax.swing.JLabel();
-JTextField textF = new JTextField(); 
-JButton send2 = new JButton();
+    javax.swing.JLabel jLabel2 = new javax.swing.JLabel();
+    javax.swing.JLabel jLabel3 = new javax.swing.JLabel();
+    JTextField textF = new JTextField();
+    JButton send2 = new JButton();
+
     public void setjLabel3(String text) {
         jLabel3.setText(text);
         window.revalidate();
     }
-    JFrame window = new JFrame(); 
-    public void AddCanvas(BoardCanvas canvas){
-         window.getContentPane().add(canvas);
-        
+
+    JFrame window = new JFrame();
+
+    public void AddCanvas(BoardCanvas canvas) {
+        window.getContentPane().add(canvas);
+
     }
-    public void setLabel1(String name){
+
+    public void setLabel1(String name) {
         jLabel1.setText(name);
     }
-    public void setLabel2(String name){
+
+    public void setLabel2(String name) {
         jLabel2.setText(name);
     }
+
     public GameWindow() {
-        
-       
+
+
         // setting closing operation 
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-  
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         // setting size of the pop window 
         window.setBounds(30, 30, 1300, 1000);
         window.setResizable(false);
 
-      //  jLabel1.setText("");
-     //   window.add(jLabel1);
+        //  jLabel1.setText("");
+        //   window.add(jLabel1);
         jLabel3.setText(" VS ");
-        
+
         jLabel3.setBounds(1000, 100, 200, 600);
         jLabel3.setOpaque(true);
         jLabel3.setBackground(Color.WHITE);
         jLabel3.setAutoscrolls(true);
         jLabel3.setBorder(new MatteBorder(null));
         jLabel3.setVerticalAlignment(TOP);
-        
-       window.add(jLabel3);
-     //   jLabel2.setText("");
-     Dimension size2 = textF.getPreferredSize();
-        textF.setBounds(1000, 750, 200, size2.height );
-        
+
+        window.add(jLabel3);
+        //   jLabel2.setText("");
+        Dimension size2 = textF.getPreferredSize();
+        textF.setBounds(1000, 750, 200, size2.height);
+
         send2.setText("Send Message");
         Dimension size = send2.getPreferredSize();
         send2.setBounds(1050, 780, size.width, size.height);
-        
+
         window.add(send2);
         window.add(textF);
         window.add(jLabel2);
         window.setVisible(true);
-        
+
     }
-    
+
 }
--- src/chessrmi/Start.java
+++ src/chessrmi/Start.java
@@ -1,2 +1,9 @@
-package chessrmi;public class Start {
+package chessrmi;
+
+public class Start {
+    public static void main(String[] args) {
+        GameWindow sWindow = GameWindow.getInstance();
+        Board chessBoard = new Board();
+       //chessBoard.paint();
+    }
 }
